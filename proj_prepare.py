69d852bcdcf47798cea3eb045bbcd2ba166044d4
Orion Wills <orion.wills@gmail.com>
2019-04-23 13:29:33 -0500

    acquire, prepare, csvs, and initial data capture

---
+++ proj_prepare.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import proj_acquire as aq
+
+
+def set_date_times(foods,activ,log):
+
+    # df1,df2,df3 = aq.acquire_fitbit()
+    foods.index= pd.to_datetime(foods.index)
+    activ.index= pd.to_datetime(activ.index)
+    log.index= pd.to_datetime(log.index)
+
+    return [foods,activ,log]
+
+def merge_dfs(foods,activ,log):
+    merged = pd.merge(activ.reset_index(),log.reset_index(),how='right')
+    merged = pd.merge(merged,foods.reset_index(),how='left')
+    return merged
+
+def prepare_fitbit():
+    return merge_dfs(*set_date_times(*aq.acquire_fitbit()))
